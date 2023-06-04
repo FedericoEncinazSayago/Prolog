a7c16296de9980d2f5e99a6b9a80353987544490
Federico Nahuel Encinaz Sayago <101824879+FedericoEncinazSayago@users.noreply.github.com>
2023-06-03 22:04:09 -0300

    Update TPHoroscopo.pl

--- Trabajos/Individuales/TPHoroscopo.pl
+++ Trabajos/Individuales/TPHoroscopo.pl
@@ -1,9 +1,10 @@
-% Hechos: Personas y signos
+% Hechos: Personas, signos y meses
 
 nacio(nacho, bsas, 26, 5, 1989).
-nacio(tito, bsas, 26, 4, 2000).
-nacio(tita, bsas, 27, 4, 2000).
+nacio(tito, bsas, 20, 6, 2000).
+nacio(tita, bsas, 10, 6, 2000).
 nacio(lola, bsas, 1, 1, 2001).
+nacio(federico, bsas, 30, 04, 2002).
 
 signoPdeP(funcional, 28, 3, 12, 4).
 signoPdeP(funcional, 12, 4, 29, 5).
@@ -13,12 +14,26 @@ signoPdeP(objetos, 22, 8, 26, 9).
 signoPdeP(objetos, 26, 9, 31, 10).
 signoPdeP(objetos, 31, 10, 28,11).
 
+mes(1, 31).
+mes(2, 28).
+mes(2, 29).
+mes(3, 31).
+mes(4, 30).
+mes(5, 31).
+mes(6, 30).
+mes(7, 31).
+mes(8, 31).
+mes(9, 30).
+mes(10, 31).
+mes(11, 30).
+mes(12, 31).
+
 % Regla: De qué signo es una persona?
 
 horoscopoPdep(Nombre, Signo) :- 
     nacio(Nombre, _, DiaDeNac, MesDeNac, _),
     signoPdeP(Signo, DiaInico, MesInicio, DiaFinal, MesFinal),
     (
-        (MesDeNac = MesInicio, DiaDeNac >= DiaInico) ;
+        (MesDeNac = MesInicio, mes(MesDeNac, CantDias), DiaDeNac >= DiaInico, DiaDeNac =< CantDias) ;
         (MesDeNac = MesFinal, DiaDeNac =< DiaFinal)
     ).
